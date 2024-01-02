4286188b3c9e203a6ac7d29941488c91a2f78a15
User <etnaude11@gmail.com>
2024-01-02 19:52:52 +0000

    4th change

--- src/main.py
+++ src/main.py
@@ -2,16 +2,12 @@ import random
 import string
 import uuid
 
-myfile = "b.txt" 
-
 filename = str(uuid.uuid4())
 password = ''.join(random.choice(string.printable) for i in range(20))
 
-file = open(myfile.replace("b", filename), "x", encoding="utf-8")
-    
-file = open(myfile.replace("b", filename), "w", encoding="utf-8")
+file = open(f"{filename}.txt", "x", encoding="utf-8")
 file.write(password)
 
-file = open(myfile.replace("b", filename), "r", encoding="utf-8")
+file = open(f"{filename}.txt", "r", encoding="utf-8")
 
 print(file.read())
